57a506e4693e1e6600783e6a8b921f281bbf1104
kaungyemyintmo <kaungyem.23@ichat.sp.edu.sg>
2024-10-30 14:24:49 +0800

    init repo

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+// Rule options:
+// "off" or 0 - turn the rule off
+// "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code)
+// "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // "no-unused-vars": "error",
+      // "no-undef": "error"
+      "no-unused-vars": "error",
+      "no-undef": 0, 
+      "no-console": "warn",
+      "curly": "error",
+      "quotes": ["error", "single"],
+    }
+  }
+];
